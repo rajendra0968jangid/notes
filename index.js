043c84ede1864c57d09811b350171739b672ad7d
Rajendra jangid <120442903+rajendra0968jangid@users.noreply.github.com>
2025-08-19 07:56:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+function copyToClipboard(ip) {
+    navigator.clipboard.writeText(ip).then(() => {
+        alert("Copied: " + ip);
+    });
+}
+
+
+// List of IPs (this can come from API, backend, or config)
+const ipAddresses = [
+    "54.196.85.7",
+    "52.90.224.135",
+    "107.23.206.11",
+    "54.84.17.222",
+    "98.80.244.231"
+];
+
+// Render IP list into the container
+const ipGrid = document.getElementById("ipGrid");
+ipAddresses.forEach(ip => {
+    const div = document.createElement("div");
+    div.classList.add("ip-item");
+    div.innerHTML = `
+    ${ip} 
+    <button class="copy-btn" onclick="copyToClipboard('${ip}')">
+      <i class="fas fa-copy"></i> Copy
+    </button>
+  `;
+    ipGrid.appendChild(div);
+});
+
+// Copy to clipboard function
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text).then(() => {
+        alert(`Copied: ${text}`);
+    }).catch(err => {
+        console.error("Copy failed", err);
+    });
+}
