1b1a00a8797f7b0cd031f7fe2c0fe3e628b5ffee
Rajendra jangid <120442903+rajendra0968jangid@users.noreply.github.com>
2025-08-27 15:07:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ function copyToClipboard(ip) {
 
 // List of IPs (this can come from API, backend, or config)
 const ipAddresses = [
-"3.81.94.182",
-    "13.220.223.181",
-    "98.85.250.114",
-    "54.167.223.245",
-    "107.20.57.222"
+"3.95.254.110",
+    "54.162.205.29",
+    "18.232.166.103",
+    "107.22.10.255",
+    "3.82.50.131"
 ];
 const v = "13:50:00"
  const startTime = new Date(`2025-08-27T${v}`).getTime();
@@ -23,7 +23,7 @@ ipAddresses.forEach(ip => {
     div.classList.add("ip-item");
     div.innerHTML = `
     ${ip} 
-    <button class="copy-btn" onclick="copyToClipboard('ssh ubuntu@${ip}')">
+    <button class="copy-btn" onclick="copyToClipboard('ssh ec2-user@${ip}')">
       <i class="fas fa-copy"></i> Copy
     </button>
   `;
